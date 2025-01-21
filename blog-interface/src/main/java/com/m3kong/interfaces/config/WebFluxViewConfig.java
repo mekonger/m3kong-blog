47dd3a86aa876c62f32b0fa8040afd72d122b53e
mekonger <m3kong.dev@gmail.com>
2025-01-21 21:02:34 +0700

    Config resources in webflux + update home page

--- blog-interface/src/main/java/com/m3kong/interfaces/config/WebFluxViewConfig.java
+++ blog-interface/src/main/java/com/m3kong/interfaces/config/WebFluxViewConfig.java
@@ -2,6 +2,7 @@ package com.m3kong.interfaces.config;
 
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
 import org.springframework.web.reactive.config.ViewResolverRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.thymeleaf.spring6.view.reactive.ThymeleafReactiveViewResolver;
@@ -15,5 +16,12 @@ public class WebFluxViewConfig implements WebFluxConfigurer {
   @Override
   public void configureViewResolvers(ViewResolverRegistry registry) {
     registry.viewResolver(thymeleafReactiveViewResolver);
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    // Cấu hình đường dẫn static resources
+    registry.addResourceHandler("/css/**")
+            .addResourceLocations("classpath:/static/css/");
   }
 }
